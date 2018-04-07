cea8bd7305dfedbd252788fdaef27d0ff179eabe
SquareFong <fanghaocheng2008@126.com>
2018-04-08 00:33:16 +0800

    finish GA algorithm on KnapsackProblem

--- GeneticAlgorithms/GeneticAlgorithms.py
+++ GeneticAlgorithms/GeneticAlgorithms.py
@@ -5,8 +5,11 @@ class GeneticAlgorithm:
     def __init__(self, data):
         self.data = data
         self.population = []
+        self.wheel = []
+        self.population_size = 0
 
     def initialize_population(self, population_size):
+        self.population_size = population_size
         data_size = len(self.data)
         if len(self.population) != 0:
             self.population.clear()
@@ -17,3 +20,54 @@ class GeneticAlgorithm:
             self.population.append(individual)
         return self.population
 
+    def roulette_wheel(self, all_fitness):
+        #all_fitness.sort(key=lambda x: x[2], reverse=True)
+        wheel = []
+        total_fitness = 0.0
+        for i in all_fitness:
+            total_fitness += i[2]
+        f = 0.0
+        for i in all_fitness:
+            f += i[2]
+            wheel.append(f/total_fitness)
+        self.wheel = wheel
+        return wheel
+
+    def pickup_individual(self, wheel):
+        s = random.random()
+        i, j = 0, len(wheel)-1
+        while i + 1 < j:
+            m = int((i + j) / 2)
+            if wheel[m] >= s:
+                j = m
+            else:
+                i = m
+        return self.population[i]
+
+    def update_population(self, generations):
+        i = generations
+        while i > 0:
+            i -= 1
+            individual1, individual2 = self.pickup_individual(self.wheel), self.pickup_individual(self.wheel)
+            cut = random.randint(0,len(self.population[0]) - 1)
+            self.population.append(individual1[0:cut] + individual2[cut:])
+            self.population.append(individual2[0:cut] + individual1[cut:])
+        return self.population
+
+    def next_generation(self, all_fitness):
+        pop_size = self.population_size
+        self.roulette_wheel(all_fitness)
+        pop = []
+        while pop_size > 0:
+            pop.append(self.pickup_individual(self.wheel))
+            pop_size -= 1
+        self.population = pop
+        return self.population
+
+    def get_best(self, all_fitness):
+        max_val, max_val_i = 0.0, 0
+        for i in range(0, len(self.wheel)-1):
+            if all_fitness[i][2] > max_val:
+                max_val = all_fitness[i][2]
+                max_val_i = i
+        return self.population[max_val_i] + [all_fitness[max_val_i],]
--- GeneticAlgorithms/KnapsackProblem.py
+++ GeneticAlgorithms/KnapsackProblem.py
@@ -1,5 +1,5 @@
 from GeneticAlgorithms import GeneticAlgorithm as ga
-
+import math
 
 with open("/home/squarefong/Documents/背包问题.txt", 'r') as f:
     data = f.read().split()
@@ -7,7 +7,7 @@ with open("/home/squarefong/Documents/背包问题.txt", 'r') as f:
 things = []
 
 
-maxVolume = data[0]
+maxVolume = int(data[0])
 
 for element in range(1, len(data), 2):
     p = []
@@ -17,9 +17,63 @@ for element in range(1, len(data), 2):
 
 GA = ga(things)
 
-ps = GA.initialize_population(100)
+GA.initialize_population(500)
 
-for i in ps:
-    print(i)
 
+all_fitness = []
+
+
+def fitness(population, all_fitness):
+    all_fitness.clear()
+    for ind in range(0, len(population)):
+        val = 0
+        vol = 0
+        individual = population[ind]
+        for i in range(0, len(individual)):
+            if individual[i] == 1:
+                vol += things[i][0]
+                val += things[i][1]
+        if vol > maxVolume:
+            val = math.log(val+1, 10)
+        p = [ind, vol, val]
+        all_fitness.append(p)
+
+
+generation = 100
+best = []
+
+
+def is_over(best):
+    max_length = len(best)-1
+    if max_length > 4:
+        if best[max_length][-1][2]==best[max_length - 1][-1][2] \
+                and best[max_length-1][-1][2]==best[max_length-2][-1][2] \
+                and best[max_length - 3][-1][2] == best[max_length - 4][-1][2] \
+                and best[max_length - 4][-1][2] == best[max_length - 5][-1][2]:
+            return True
+    return False
+
+
+gen = generation
+while gen > 0:
+    gen -= 1
+    fitness(GA.population, all_fitness)
+    GA.roulette_wheel(all_fitness)
+    best.append(GA.get_best(all_fitness))
+    if is_over(best) and best[-1][-1][1] < maxVolume:
+        print("在经过{}次迭代后找到可行的解".format(generation-gen))
+        print(best[-1][0:len(best[-1])-1],end=" ")
+        print("总重量 {} 总价值 {}".format(best[-1][-1][1],best[-1][-1][2]))
+        break
+    GA.update_population(100)
+    fitness(GA.population, all_fitness)
+    GA.roulette_wheel(all_fitness)
+    GA.next_generation(all_fitness)
+
+if gen == 0:
+    print("fail to find best choose")
+#for i in pop:
+#    print(i)
+
+#print(all_fitness)
 # print(things)
